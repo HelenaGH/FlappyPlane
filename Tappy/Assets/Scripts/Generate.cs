68f6efebb89af673cac9f5a2247906f9167ac7dc
HelenaGH <47184541+HelenaGH@users.noreply.github.com>
2020-05-06 12:13:38 +0200

    done

---
+++ Tappy/Assets/Scripts/Generate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Generate : MonoBehaviour 
+{
+	public GameObject rocks;
+	int result = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating("CreateRocks", 1f, 1.5f);
+	}
+	
+	void OnGUI()
+	{
+		GUI.color = Color.black;
+		GUILayout.Label("Result: " + result.ToString());
+	}
+
+	// Update is called once per frame
+	void CreateRocks () 
+	{
+		Instantiate(rocks);
+	}
+}
